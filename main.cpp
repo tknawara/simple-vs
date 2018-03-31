55433292526489e15fb6e92efa3bd63f4f4a4897
tarek-nawara <tarek.nawara@gmail.com>
2018-03-31 21:15:32 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+ * File: main.cpp
+ * Description: Application entry point
+ * Created at: 2018-3-31
+ */
+
+#include <iostream>
+
+int main() {
+  std::cout << "hello world" << '\n';
+  return 0;
+}
